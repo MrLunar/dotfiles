65cc42e249edeba891627464e9a413d3ca456dbc
Mark Bowker <860189+MrLunar@users.noreply.github.com>
2024-01-03 19:17:03 +0000

    Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,21 +7,21 @@ config.color_scheme = 'Monokai Pro (Gogh)'
 config.colors = {
     background = '222222'
 }
-config.default_cursor_style = 'BlinkingBar'
 config.font = wezterm.font('SauceCodePro NF Medium')
+config.force_reverse_video_cursor = true
 config.initial_cols = 110
 config.initial_rows = 25
-config.launch_menu = {}
 
+config.launch_menu = {}
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     table.insert(config.launch_menu, {
         label = 'PowerShell',
         args = { 'powershell.exe', '-NoLogo' },
     })
-    -- table.insert(config.launch_menu, {
-    --     label = 'Ubuntu 22.04',
-    --     args = { 'ubuntu2204.exe' },
-    -- })
+    table.insert(config.launch_menu, {
+        label = 'Git Bash',
+        args = { 'C:/Program Files/Git/bin/bash.exe' },
+    })
 end
 
 return config
